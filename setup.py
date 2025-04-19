8d7992df55c2f12b5774b09208cb981c568e3088
ArielMartin-Tech4Life <Tech4LifeandBeyond@gmail.com>
2025-04-18 20:04:52 -0400

    added package_data line

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     description="SDK for validating Kivai protocol commands",
     author="OpenKivai Community",
     packages=find_packages(),
+    package_data={
+        'kivai_sdk': ['schema/kivai-command.schema.json'],
+    },
     install_requires=["jsonschema"],
     keywords=["kivai", "validator", "jsonschema", "iot", "commands"],
     classifiers=[
@@ -13,3 +16,4 @@ setup(
         "License :: OSI Approved :: MIT License"
     ],
 )
+
