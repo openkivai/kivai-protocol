7d1c6124bb3598c3c8210871895928cc2badab0f
ArielMartin-Tech4Life <Tech4LifeandBeyond@gmail.com>
2025-04-18 21:18:15 -0400

    Bump version to 0.1.3 for PyPI release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kivai_sdk",
-    version="0.1.2",
+    version="0.1.3",
     description="SDK for validating Kivai protocol commands",
     author="OpenKivai Community",
     packages=find_packages(),
